6c879506a3158ba275811d885c42b5f80f21877c
tudoistube <tudoistube@ymail.com>
2019-11-02 09:50:10 +0900

    added Button.

--- cs0020_winform/Form1.Designer.cs
+++ cs0020_winform/Form1.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
@@ -41,11 +42,22 @@
             this.label1.TabIndex = 0;
             this.label1.Text = "Git을 사용합니다.";
             // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("굴림", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.button1.Location = new System.Drawing.Point(292, 190);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(163, 66);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Git OK";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -57,6 +69,7 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
     }
 }
 
